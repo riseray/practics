facfd53be84c68af5797da55303bb271ed9e377d
Lee <riseray@hotmail.com>
2015-06-10 15:15:50 -0400

    update countwords.php

--- countWords.php
+++ countWords.php
@@ -9,5 +9,7 @@ $expode
         $strArr = preg_split("/[\s,.)(]+/", $str);
         return $strArr;
     }
+
+echo "end";
 //End
 //Write a function countWords($str) that takes any string of characters and returns the number of times each word occurs. You should ignore the distinction between capital and lowercase letters, and do not have to worry about dealing with characters that are not letters. Write additional code to call the function and print out the result.
