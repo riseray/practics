99cb1c46c27a42e42dda26b1583128eb3a934c27
Lee <riseray@hotmail.com>
2015-06-10 15:10:59 -0400

    initial commit

---
+++ countWords.php
@@ -0,0 +1,13 @@
+<?php
+    $str = "Write a function countWords() that takes any string of characters and returns the number of times each word occurs. You should ignore the distinction between capital and lowercase letters, and do not have to worry about dealing with characters that are not letters. Write additional code to call the function and print out the result";
+    countWords($str);
+    print_r(array_count_values(countWords($str)));
+
+$expode
+    function countWords($str){
+        $str = strtolower($str);
+        $strArr = preg_split("/[\s,.)(]+/", $str);
+        return $strArr;
+    }
+//End
+//Write a function countWords($str) that takes any string of characters and returns the number of times each word occurs. You should ignore the distinction between capital and lowercase letters, and do not have to worry about dealing with characters that are not letters. Write additional code to call the function and print out the result.
---
+++ wordReplace.php
@@ -0,0 +1,22 @@
+<?php
+    $oldWord = "word";
+    $newWord = "wordIsReplaced";
+    $filePath = "wordReplace.txt";
+    wordReplace($filePath, $oldWord, $newWord);
+
+	function wordReplace($filePath, $oldWord, $newWord){
+		$handler = fopen($filePath,"r");
+		$fileStr = fread($handler, filesize($filePath));
+        fclose($handler);
+        $fileStrToWrite = str_ireplace($oldWord, $newWord, $fileStr);
+		$handler = fopen($filePath,"w");
+        fwrite($handler, $fileStrToWrite);
+        fclose($handler);
+	}
+//END
+/*
+Write a function that takes a filepath(with filename), a
+string (word to find) and another string (word to use for replacement).
+The function must find the first word (2nd arg) in the file and
+replace all its occurrences with the other word (3rd arg).
+*/
